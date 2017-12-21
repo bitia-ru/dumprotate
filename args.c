941d41b105070f98c09a29f22a4d8a65a19d4fc4
Svetlana Batova <sbatova@bitia.ru>
2017-12-22 00:06:43 +0300

    update parse_args

--- args.c
+++ args.c
@@ -3,6 +3,10 @@
 
 int parse_args(Dumprotate* drd, int argc, char** argv) {
     char currentFlag = '0';
+    int res;
+    off_t numOfBytesFinal;
+    
+    //drd->maxCount = 5;
     for (int i = 1; i < argc; i++)
     {
         if (currentFlag == '0')
@@ -14,19 +18,11 @@ int parse_args(Dumprotate* drd, int argc, char** argv) {
             switch (argv[i][1])  
             {  
                case 's':  
-                  currentFlag = 's';  
-                  break;  
-               case 'n':  
-                  currentFlag = 'n'; 
-                  break;
-               case 'e':  
-                  currentFlag = 'e'; 
-                  break;
-               case 'c':  
-                  currentFlag = 'c'; 
-                  break;
+               case 'n':
+               case 'e':
+               case 'c':
                case 'd':  
-                  currentFlag = 'd'; 
+                  currentFlag = argv[i][1]; 
                   break;
                case 'h':  
                   return 0;
@@ -38,41 +34,113 @@ int parse_args(Dumprotate* drd, int argc, char** argv) {
         {
             switch (currentFlag)  
             {  
-               case 's':  
-                  if (argv[i][strlen(argv[i])-1] > 9)
+               case 's': 
+                  res = check_argument(&numOfBytesFinal, argv[i]);
+                  if (res != 0)
                   {
-                      switch (argv[i][strlen(argv[i])-1])
-                      {
-                          case 'k':
-                          case 'K':
-                              argv[i][strlen(argv[i])-1] = 0;
-                              printf("%d\n",atoi(argv[i])*1024);
-                              break;
-                          case 'm':
-                          case 'M':
-                              break;
-                          case 'g':
-                          case 'G':
-                              break;
-                          default:  
-                              return 1;
-                      }
+                      return 1;
+                  }
+                  else
+                  {
+                      drd->args.maxSize = numOfBytesFinal;
                   } 
                   break;  
-               case 'n':  
-                  currentFlag = 'n'; 
+               case 'n': 
+                  res = is_number(argv[i]);
+                  if (res != 0)
+                  {
+                      return 1;
+                  }
+                  else
+                  {
+                      drd->args.maxCount = atoi(argv[i]);
+                  }
                   break;
                case 'e':  
-                  currentFlag = 'e'; 
+                  res = check_argument(&numOfBytes_final, argv[i]);
+                  if (res != 0)
+                  {
+                      return 1;
+                  }
+                  else
+                  {
+                      drd->args.minEmptySpace = numOfBytesFinal;
+                  }
                   break;
                case 'c':  
-                  currentFlag = 'c'; 
+                  drd->args.configPath = argv[i];
                   break;
                case 'd':  
-                  currentFlag = 'd'; 
+                  drd->args.dumpDir = argv[i];
                   break;
             } 
             currentFlag = '0'; 
+        }
+    }
+    return 0;
+}
+int is_number(char* param) {
+    for (int i = 0; i < strlen(param); i++)
+    {
+        if ((param[i] > '9') || (param[i] < '0'))
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+int convert_to_bytes(off_t* numOfBytes, char* arg) {
+    int res;
+    char c;
+    
+    c = arg[strlen(arg)-1];
+    arg[strlen(arg)-1] = 0;
+    res = is_number(arg);
+    if (res != 0)
+    {
+        return 1;
+    }
+    switch (c)
+    {
+        case 'k':
+        case 'K':
+            numOfBytes[0] = atoi(arg)*1024;
+            printf("%d\n",atoi(arg)*1024);
+            return 0;
+        case 'm':
+        case 'M':
+            numOfBytes[0] = atoi(arg)*1024*1024;
+            return 0;
+        case 'g':
+        case 'G':
+            numOfBytes[0] = atoi(arg)*1024*1024*1024;
+            return 0;
+        default:  
+            return 1;
+    }
+}
+int check_argument(off_t* numOfBytesFinal, char* arg) {
+    int res;
+    off_t numOfBytes;
+    if ((arg[strlen(arg)-1] > '9') || (arg[strlen(arg)-1] < '0'))
+    {
+        res = convert_to_bytes(&numOfBytes, arg);
+        if (res != 0)
+        {
+            return 1;
+        }
+        numOfBytesFinal[0] = numOfBytes;
+    }
+    else
+    {
+        res = is_number(arg);
+        if (res != 0)
+        {
+            return 1;
+        }
+        else
+        {
+            numOfBytesFinal[0] = atoi(arg);
         }
     }
     return 0;
