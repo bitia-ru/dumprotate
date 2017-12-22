61c0b10b061d61acfdf9fe6d4a9d9727aa5da2de
Svetlana Batova <sbatova@bitia.ru>
2017-12-22 18:32:08 +0300

    change isdigit to sscanf

--- args.c
+++ args.c
@@ -1,18 +1,15 @@
+#include <stdio.h> // sscanf
 #include <string.h> // strlen
-#include <math.h> // pow
-#include <stdlib.h> //atoi
-#include <ctype.h> // isdigit
 
 #include "dumprotate.h"
 
-int is_number(char* param);
-int convert_to_bytes(off_t* numOfBytes, char* arg);
-int check_argument(off_t* numOfBytesFinal, char* arg);
+int convert_to_bytes(char* str, off_t* numOfBytes);
 
 int parse_args(Dumprotate* drd, int argc, char** argv) {
     char currentFlag = '0';
     int res;
-    off_t numOfBytesFinal;
+    off_t numOfBytes;
+    int val;
 
     for (int i = 1; i < argc; i++) {
         if (currentFlag == '0') {
@@ -36,27 +33,27 @@ int parse_args(Dumprotate* drd, int argc, char** argv) {
         } else {
             switch (currentFlag) {
                 case 's':
-                    res = check_argument(&numOfBytesFinal, argv[i]);
+                    res = convert_to_bytes(argv[i], &numOfBytes);
                     if (res != 0) {
                         return 1;
                     } else {
-                        drd->args.maxSize = numOfBytesFinal;
+                        drd->args.maxSize = numOfBytes;
                     }
                     break;
                 case 'n':
-                    res = is_number(argv[i]);
-                    if (res != 0) {
+                    res = sscanf(argv[i], "%d", &val);
+                    if (res != 1) {
                         return 1;
                     } else {
-                        drd->args.maxCount = atoi(argv[i]);
+                        drd->args.maxCount = val;
                     }
                     break;
                 case 'e':
-                    res = check_argument(&numOfBytesFinal, argv[i]);
+                    res = convert_to_bytes(argv[i], &numOfBytes);
                     if (res != 0) {
                         return 1;
                     } else {
-                        drd->args.minEmptySpace = numOfBytesFinal;
+                        drd->args.minEmptySpace = numOfBytes;
                     }
                     break;
                 case 'c':
@@ -72,61 +69,33 @@ int parse_args(Dumprotate* drd, int argc, char** argv) {
     return 0;
 }
 
-int is_number(char* param) {
-    for (int i = 0; i < strlen(param); i++) {
-        if (isdigit(param[i]) == 0) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int convert_to_bytes(off_t* numOfBytes, char* arg) {
+int convert_to_bytes(char* str, off_t* numOfBytes) {
+    off_t i;
+    char a;
     int res;
-    char c;
-    int len;
-
-    len = strlen(arg) - 1;
-    c = arg[len];
-    arg[len] = 0;
-    res = is_number(arg);
-    if (res != 0) {
-        return 1;
-    }
-    switch (c) {
-        case 'k':
-        case 'K':
-            *numOfBytes = atoi(arg)*1024;
+    res = sscanf(str, "%ld%c", &i, &a);
+    switch (res) {
+        case 1:
+            *numOfBytes = i;
             return 0;
-        case 'm':
-        case 'M':
-            *numOfBytes = atoi(arg) * pow(1024.0, 2);
-            return 0;
-        case 'g':
-        case 'G':
-            *numOfBytes = atoi(arg) * pow(1024.0, 3);
-            return 0;
+        case 2:
+            switch (a) {
+                case 'k':
+                case 'K':
+                    *numOfBytes = i * 1024;
+                    return 0;
+                case 'm':
+                case 'M':
+                    *numOfBytes = i * (1024 << 1);
+                    return 0;
+                case 'g':
+                case 'G':
+                    *numOfBytes = i * (1024 << 2);
+                    return 0;
+                default:
+                    return 1;
+            }
         default:
             return 1;
     }
-}
-
-int check_argument(off_t* numOfBytesFinal, char* arg) {
-    int res;
-    off_t numOfBytes;
-    if (isdigit(arg[strlen(arg) - 1]) == 0) {
-        res = convert_to_bytes(&numOfBytes, arg);
-        if (res != 0) {
-            return 1;
-        }
-        *numOfBytesFinal = numOfBytes;
-    } else {
-        res = is_number(arg);
-        if (res != 0) {
-            return 1;
-        } else {
-            *numOfBytesFinal = atoi(arg);
-        }
-    }
-    return 0;
 }
