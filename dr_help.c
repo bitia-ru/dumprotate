74c9e862073011e075818200d09eb13d2a79f3f8
ALev <alev@bitia.ru>
2017-12-26 11:55:07 +0300

    Help simplified.

--- dr_help.c
+++ dr_help.c
@@ -6,19 +6,13 @@ int dr_help() {
     puts("Usage: dumprot [OPTION...]");
     puts("");
     puts("\t-h, --help\tdisplay this help and exit");
-    puts("\t-s\t\tlimit on the maximum storage size");
-    puts("\t\t\t\tSIZE — number of bytes,");
-    puts("\t\t\t\tSIZEk/SIZEK — number of KB,");
-    puts("\t\t\t\tSIZEm/SIZEM — number of MB,");
-    puts("\t\t\t\tSIZEg/SIZEG — number of GB");
-    puts("\t-n\t\tlimit on the number of dumps");
-    puts("\t-e\t\tlimit on the minimum amount of free space in the mount point");
-    puts("\t\t\t\tSIZE — number of bytes,");
-    puts("\t\t\t\tSIZEk/SIZEK — number of KB,");
-    puts("\t\t\t\tSIZEm/SIZEM — number of MB,");
-    puts("\t\t\t\tSIZEg/SIZEG — number of GB");
-    puts("\t-c\t\tconfig file path");
-    puts("\t-d\t\tdump directory path");
+    puts("\t-s SIZE\t\tlimit on the maximum storage size");
+    puts("\t\t\t\tSIZE — number of bytes (kKmMgG supported)");
+    puts("\t-n NUM\t\tlimit on the number of dumps");
+    puts("\t-e SIZE\t\tlimit on the minimum amount of free space in the mount point");
+    puts("\t\t\t\tSIZE — number of bytes (kKmMgG supported)");
+    puts("\t-c PATH\t\tconfig file path");
+    puts("\t-d DIR\t\tdump directory path");
     puts("");
     return 0;
 }
